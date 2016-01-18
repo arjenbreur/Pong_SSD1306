3a4c6ec03e535270a2dad489f699e299b69013f4
Arjen Breur <arjenbreur@gmail.com>
2016-01-18 17:45:48 +0100

    initial code commit.

---
+++ Eriestuff_Pong_SSD1306.cpp
@@ -0,0 +1,31 @@
+/*
+ Eriestuff_Pong_SSD1306, 
+ Library for displaying and playing Pong on an Adafruit SSD1306 128x64 OLED display.
+ Created by Arjen Breur, Jan 2016.
+ Code adapted from: http://michaelteeuw.nl/post/87381052117/building-pong
+ */
+
+#ifndef Eriestuff_Pong_SSD1306_h
+#define Eriestuff_Pong_SSD1306_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class Eriestuff_Pong_SSD1306
+{
+  public:
+	Eriestuff_Pong_SSD1306();
+	
+  private:
+
+};
+
+#endif
+
+/* This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
+ To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send 
+ a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+*/
---
+++ README.md
@@ -0,0 +1,6 @@
+Pong_SSD1306 (for Arduino)
+=======
+ Pong_SSD1306 
+ Library for displaying and playing Pong on an Adafruit SSD1306 128x64 OLED display.
+ Created by Arjen Breur, Jan 2016.
+ Code adapted from: http://michaelteeuw.nl/post/87381052117/building-pong
