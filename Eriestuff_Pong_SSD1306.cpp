e4490491ac53c3fff0259e638e2e33e7bb9483b0
Arjen Breur <arjenbreur@gmail.com>
2016-01-19 22:39:01 +0100

    Compiles. Not tested.

--- Eriestuff_Pong_SSD1306.cpp
+++ Eriestuff_Pong_SSD1306.cpp
@@ -1,31 +1,350 @@
 /*
- Eriestuff_Pong_SSD1306, 
+ Eriestuff_Pong_SSD1306 
  Library for displaying and playing Pong on an Adafruit SSD1306 128x64 OLED display.
  Created by Arjen Breur, Jan 2016.
  Code adapted from: http://michaelteeuw.nl/post/87381052117/building-pong
+
+ *** Manual setup required ***
+ Open the Adafruit_SSD1306.h file, and add these lines to the top of the file:
+
+    #ifndef Adafruit_SSD1306_h
+    #define Adafruit_SSD1306_h
+    
+ Add this line at the end of the file:
+ 
+    #endif
+    
+  This is standard practise Arduino code to prevent libraries from being included twice. 
+ 
  */
 
-#ifndef Eriestuff_Pong_SSD1306_h
-#define Eriestuff_Pong_SSD1306_h
+#include <Adafruit_SSD1306.h>
+#include <SmoothAnalogInput.h>
+#include "Eriestuff_Pong_SSD1306.h"
 
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
+// Constructor - Call this *before* the Arduino Setup() function
+Eriestuff_Pong_SSD1306::Eriestuff_Pong_SSD1306(Adafruit_SSD1306& display, SmoothAnalogInput& controlA, SmoothAnalogInput& controlB) : _display(display), _controlA(controlA), _controlB(controlB) {
+    init();
+    winningScore = 21;
+    _isPlaying = false;
+}
 
-class Eriestuff_Pong_SSD1306
-{
-  public:
-	Eriestuff_Pong_SSD1306();
-	
-  private:
+// PUBLIC METHODS //////////////////////////////////////////////////
 
-};
+// Loop - Call this method from within the Arduino Loop() function
+void Eriestuff_Pong_SSD1306::loop(){
+    if(_isPlaying){
+        if(max(scoreA, scoreB) >= winningScore){
+            gameOver();
+        }else{
+            if(pongCountDown == 0){
+                calculateMovement();
+                draw();
+                delay(LOOP_DELAY);
+            }else{
+                // abuse scoreboard for countdown
+                scoreA = pongCountDown;
+                scoreB = pongCountDown;
+                draw();
+                pongCountDown--;
+                delay(1000);
+            }
+        }
+    }
+}
 
-#endif
+bool Eriestuff_Pong_SSD1306::isPlaying(){
+    return _isPlaying;
+}
+bool Eriestuff_Pong_SSD1306::isGameOver(){
+    return _isGameOver;
+}
 
-/* This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
- To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send 
- a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-*/
+void Eriestuff_Pong_SSD1306::startGame(){
+    _isPlaying = true;
+    _isGameOver = false;
+    initNewGame();
+    wipeScreen();
+}
+void Eriestuff_Pong_SSD1306::pauseGame(){
+    _isPlaying = false;
+}
+void Eriestuff_Pong_SSD1306::resumeGame(){
+    _isPlaying = true;
+}
+void Eriestuff_Pong_SSD1306::stopGame(){
+    _isPlaying = false;
+    wipeScreen();
+}
+
+void Eriestuff_Pong_SSD1306::wipeScreen(void) {
+    for (int16_t i=0; i<_display.height()/2; i+=1) {
+        _display.drawRect(i, i, _display.width()-2*i, _display.height()-2*i, WHITE);
+        _display.display();
+    }
+    for (int16_t i=_display.height()/2; i>=-1; i-=1) {
+        _display.drawRect(i, i, _display.width()-2*i, _display.height()-2*i, BLACK);
+        _display.display();
+    }
+}
+
+// PRIVATE METHODS ////////////////////////////////////////////////
+
+void Eriestuff_Pong_SSD1306::gameOver(){
+    _isGameOver = true;
+    stopGame();
+    // display winner
+    _display.setCursor(3, 20);
+    _display.setTextSize(3);
+    if(scoreA > scoreB) _display.println("A Wins!");
+    else                _display.println("B Wins!");
+    _display.display();
+    delay(1000);
+}
+
+void Eriestuff_Pong_SSD1306::initNewGame(){
+    //reset
+    scoreA = 0;
+    scoreB = 0;
+    controlAMax = 0;    // init to minimum
+    controlAMin = 1023; // init to maximum
+    controlBMax = controlAMax;
+    controlBMin = controlAMin;
+    pongCountDown = 5;
+    paddleLocationA = 0;
+    paddleLocationB = 0;
+    lastPaddleLocationA = 0;
+    lastPaddleLocationB = 0;
+    ballX = SCREEN_WIDTH/2;
+    ballY = SCREEN_HEIGHT/2;
+    ballSpeedX = 2;
+    ballSpeedY = 1;
+}
+
+void Eriestuff_Pong_SSD1306::calculateMovement(){
+    int controlA = _controlA.read();
+    int controlB = _controlB.read();
+
+    controlAMax = max(controlAMax, controlA*0.7); // take 90% of reading as maximum reading for the controls
+    controlAMin = min(controlAMin, controlA*1.3);
+    controlBMax = max(controlBMax, controlB*0.7);
+    controlBMin = min(controlBMin, controlB*1.3);
+
+    int maxPaddleLocation = SCREEN_HEIGHT - PADDLE_HEIGHT;
+
+    paddleLocationA = map(controlA, controlAMax, controlAMin, 0, maxPaddleLocation);
+    if (paddleLocationA > maxPaddleLocation) paddleLocationA = maxPaddleLocation;
+    if (paddleLocationA < 0) paddleLocationA = 0;
+
+    boolean isRobot = true;
+    if(isRobot){
+        paddleLocationB = getRobotPaddleLocation();
+    }else{
+        paddleLocationB = map(controlB, controlBMax, controlBMin, 0, maxPaddleLocation);
+    }
+
+    if (paddleLocationB > maxPaddleLocation) paddleLocationB = maxPaddleLocation;
+    if (paddleLocationB < 0) paddleLocationB = 0;
+
+    int paddleSpeedA = paddleLocationA - lastPaddleLocationA;
+    int paddleSpeedB = paddleLocationB - lastPaddleLocationB;
+
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    //bounce from top and bottom
+    if (ballY >= SCREEN_HEIGHT - BALL_SIZE || ballY <= 0) {
+        ballSpeedY *= -1;
+        soundBounce();
+    }
+
+    //bounce from paddle A
+    if (ballX >= PADDLE_PADDING && ballX <= PADDLE_PADDING+BALL_SIZE && ballSpeedX < 0) {
+        if (ballY > paddleLocationA - BALL_SIZE && ballY < paddleLocationA + PADDLE_HEIGHT) {
+            soundBounce();
+            ballSpeedX *= -1;
+            addEffect(paddleSpeedA);
+        }
+    }
+
+    //bounce from paddle B
+    if (ballX >= SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING-BALL_SIZE && ballX <= SCREEN_WIDTH-PADDLE_PADDING-BALL_SIZE && ballSpeedX > 0) {
+        if (ballY > paddleLocationB - BALL_SIZE && ballY < paddleLocationB + PADDLE_HEIGHT) {
+        soundBounce();
+        ballSpeedX *= -1;
+        addEffect(paddleSpeedB);
+        }
+    }
+
+    //score points if ball hits wall behind paddle
+    if (ballX >= SCREEN_WIDTH - BALL_SIZE || ballX <= 0) {
+        if (ballSpeedX > 0) {
+            scoreA++;
+            ballX = SCREEN_WIDTH / 4;
+        }
+        if (ballSpeedX < 0) {
+            scoreB++;
+            ballX = SCREEN_WIDTH / 4 * 3;
+        }
+        soundPoint();   
+    }
+
+    //set last paddle locations
+    lastPaddleLocationA = paddleLocationA;
+    lastPaddleLocationB = paddleLocationB;  
+}
+
+void Eriestuff_Pong_SSD1306::draw(){
+    _display.clearDisplay(); 
+
+    //draw paddle A
+    _display.fillRect(PADDLE_PADDING,paddleLocationA,PADDLE_WIDTH,PADDLE_HEIGHT,WHITE);
+
+    //draw paddle B
+    _display.fillRect(SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING,paddleLocationB,PADDLE_WIDTH,PADDLE_HEIGHT,WHITE);
+
+    //draw center line
+    for (int i=0; i<SCREEN_HEIGHT; i+=4) {
+        _display.drawFastVLine(SCREEN_WIDTH/2, i, 2, WHITE);
+    }
+
+    //draw ball
+    _display.fillRect(ballX,ballY,BALL_SIZE,BALL_SIZE,WHITE);
+
+    //print scores
+
+    //backwards indent score A. This is dirty, but it works ... ;)
+    int scoreAWidth = 5 * FONT_SIZE;
+    if (scoreA > 9) scoreAWidth += 6 * FONT_SIZE;
+    if (scoreA > 99) scoreAWidth += 6 * FONT_SIZE;
+    if (scoreA > 999) scoreAWidth += 6 * FONT_SIZE;
+    if (scoreA > 9999) scoreAWidth += 6 * FONT_SIZE;
+
+    _display.setCursor(SCREEN_WIDTH/2 - SCORE_PADDING - scoreAWidth,0);
+    _display.print(scoreA);
+
+    _display.setCursor(SCREEN_WIDTH/2 + SCORE_PADDING+1,0); //+1 because of dotted line.
+    _display.print(scoreB);
+
+    _display.display();
+}  
+
+//add effect to the ball
+void Eriestuff_Pong_SSD1306::addEffect(int paddleSpeed){
+    float oldBallSpeedY = ballSpeedY;
+
+    //add effect to ball when paddle is moving while bouncing.
+    //for every pixel of paddle movement, add or substact EFFECT_SPEED to ballspeed.
+    for (int effect = 0; effect < abs(paddleSpeed); effect++) {
+        if (paddleSpeed > 0) {
+            ballSpeedY += EFFECT_SPEED;
+        } else {
+            ballSpeedY -= EFFECT_SPEED;
+        }
+    }
+
+    //limit to minimum speed
+    if (ballSpeedY < MIN_Y_SPEED && ballSpeedY > -MIN_Y_SPEED) {
+        if (ballSpeedY > 0) ballSpeedY = MIN_Y_SPEED;
+        if (ballSpeedY < 0) ballSpeedY = -MIN_Y_SPEED;
+        if (ballSpeedY == 0) ballSpeedY = oldBallSpeedY;
+    }
+
+    //limit to maximum speed
+    if (ballSpeedY > MAX_Y_SPEED) ballSpeedY = MAX_Y_SPEED;
+    if (ballSpeedY < -MAX_Y_SPEED) ballSpeedY = -MAX_Y_SPEED;
+}
+
+void Eriestuff_Pong_SSD1306::centerPrint(char *text, int y, int size){
+    _display.setTextSize(size);
+    _display.setCursor(SCREEN_WIDTH/2 - ((strlen(text))*6*size)/2,y);
+    _display.print(text);
+}
+
+int Eriestuff_Pong_SSD1306::getRobotPaddleLocation(){
+    int paddleSpeed = 0;
+
+    // x location of paddle face
+    int paddleX = SCREEN_WIDTH-PADDLE_WIDTH-PADDLE_PADDING;
+    // distance ball needs to travel in x direction until it hits paddle
+    int diffX = paddleX - ballX;
+
+    if(ballSpeedX < 0 || diffX <=0){
+        // ball is moving from B to A
+        // or ball is beyond paddle B
+        // paddle B should get back to center
+        if(lastPaddleLocationB*2 != SCREEN_HEIGHT){
+            // paddleSpeed = (lastPaddleLocationB*2 > SCREEN_HEIGHT)? -1:1;
+        }
+    }else{
+        // ball is moving towards paddle B
+        // try to intercept it
+        // number of iterations needed for ball to travel this distance in x direction   
+        float cyclesToGo = abs(diffX)/ballSpeedX;
+        // total distance in y direction ball will travel during these iterations (until ball hits paddle)
+        int predictedYWithoutBounce = ballY + (cyclesToGo * ballSpeedY); // will be negative when ball is moving up out of the screen.
+        // distance in y direction left, after last bounce (from TOP or BOTTOM of screen)
+        int predictedY = predictedYWithoutBounce;
+        // unless there is a bounce:
+        if(predictedYWithoutBounce > SCREEN_HEIGHT){
+            // ball will bounce (at least once) on bottom of screen
+            predictedY = SCREEN_HEIGHT - (predictedYWithoutBounce - SCREEN_HEIGHT);
+        }
+        if(predictedYWithoutBounce < 0){
+            // ball will bounce from top of screen
+            predictedY = predictedYWithoutBounce *-1;
+        }
+        // set paddleSpeed to get to the predicted y location in time
+        int diffY = abs(lastPaddleLocationB - predictedY);
+        paddleSpeed = diffY / cyclesToGo;
+        if(predictedY < lastPaddleLocationB){
+            paddleSpeed *=-1; // move up!
+        }
+    }
+  
+    // paddleSpeed is now perfect... resulting in a perfect Pong robot
+    // but we want the robot to make mistakes...
+
+    // use same random number seed while score is the same  
+    randomSeed(scoreA+scoreB);
+
+    // limit the maximum speed to 1
+    if(paddleSpeed>0) paddleSpeed = 1;
+    if(paddleSpeed<0) paddleSpeed =-1;
+
+    // start moving too late
+    int reactionDistance = random(SCREEN_WIDTH/2, SCREEN_WIDTH/3);
+    if(diffX > reactionDistance) paddleSpeed = 0;
+
+    // hesitate now and then
+    if(random(0,3) == 0) paddleSpeed = 0;
+
+    int paddleLocation = lastPaddleLocationB + paddleSpeed;
+    return paddleLocation; 
+}
+
+// SOUND METHODS //////////////////////////////////////////////////////////////
+void Eriestuff_Pong_SSD1306::soundBounce(){ beep(BEEPER, 500, 50);}
+void Eriestuff_Pong_SSD1306::soundPoint(){  beep(BEEPER, 150, 150);}
+void Eriestuff_Pong_SSD1306::soundStart(){
+    beep(BEEPER, 250, 100);
+    delay(100);
+    beep(BEEPER, 500, 100);
+    delay(100);
+    beep(BEEPER, 1000, 100);
+    delay(100);
+    noTone(BEEPER);
+}
+// the sound producing function
+// http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
+void Eriestuff_Pong_SSD1306::beep(unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds){
+    int x;   
+    long delayAmount = (long)(1000000/frequencyInHertz);
+    long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
+    for (x=0;x<loopTime;x++){  
+        digitalWrite(speakerPin,HIGH);
+        delayMicroseconds(delayAmount);
+        digitalWrite(speakerPin,LOW);
+        delayMicroseconds(delayAmount);
+        }  
+}
--- Eriestuff_Pong_SSD1306.h
+++ Eriestuff_Pong_SSD1306.h
@@ -1,10 +1,22 @@
 /*
- Eriestuff_Pong_SSD1306, 
+ Eriestuff_Pong_SSD1306 
  Library for displaying and playing Pong on an Adafruit SSD1306 128x64 OLED display.
  Created by Arjen Breur, Jan 2016.
  Code adapted from: http://michaelteeuw.nl/post/87381052117/building-pong
- */
 
+ *** Manual setup required ***
+ Open the Adafruit_SSD1306.h file, and add these lines to the top of the file:
+
+    #ifndef Adafruit_SSD1306_h
+    #define Adafruit_SSD1306_h
+    
+ Add this line at the end of the file:
+ 
+    #endif
+    
+  This is standard practise Arduino code to prevent libraries from being included twice. 
+ 
+ */
 
 #ifndef Eriestuff_Pong_SSD1306_h
 #define Eriestuff_Pong_SSD1306_h
@@ -15,18 +27,76 @@
 #include "WProgram.h"
 #endif
 
-class Eriestuff_Pong_SSD1306
-{
-  public:
-	Eriestuff_Pong_SSD1306();
+#include <Adafruit_SSD1306.h>
 
-  private:
+#define BEEPER 10
+#define CONTROL_A A1
+#define CONTROL_B A1
+#define FONT_SIZE 2
+#define SCREEN_WIDTH 127  //real size minus 1, because coordinate system starts with 0
+#define SCREEN_HEIGHT 63  //real size minus 1, because coordinate system starts with 0
+#define PADDLE_WIDTH 4
+#define PADDLE_HEIGHT 10
+#define PADDLE_PADDING 5
+#define BALL_SIZE 3
+#define SCORE_PADDING 10
+#define EFFECT_SPEED 0.5
+#define MIN_Y_SPEED 0.5
+#define MAX_Y_SPEED 2
+#define LOOP_DELAY 15
 
+class Eriestuff_Pong_SSD1306{
+    public:
+        Eriestuff_Pong_SSD1306(Adafruit_SSD1306& display, SmoothAnalogInput& controlA, SmoothAnalogInput& controlB);
+        void loop();
+        bool isPlaying();
+        bool isGameOver();
+
+        void startGame();
+        void pauseGame();
+        void resumeGame();
+        void stopGame();
+
+        void wipeScreen();
+        
+        Adafruit_SSD1306& _display;
+        SmoothAnalogInput& _controlA;
+        SmoothAnalogInput& _controlB;   
+
+    private:
+        void calculateMovement();
+        void draw();
+        void addEffect(int paddleSpeed);
+        void centerPrint(char *text, int y, int size);
+        void gameOver();
+        void initNewGame();
+        int getRobotPaddleLocation();
+
+        void soundBounce();
+        void soundPoint();
+        void soundStart();
+        void beep(unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds);
+
+        // pong globals
+        int controlAMax;
+        int controlAMin;
+        int controlBMax;
+        int controlBMin;
+        int paddleLocationA;
+        int paddleLocationB;
+        float ballX;
+        float ballY;
+        float ballSpeedX;
+        float ballSpeedY;
+        int lastPaddleLocationA;
+        int lastPaddleLocationB;
+        int scoreA;
+        int scoreB;
+        int pongCountDown;
+        int winningScore;
+        
+        bool _isPlaying;
+        bool _isGameOver;
 };
 
 #endif
-
-/* This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
- To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send 
- a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-*/
